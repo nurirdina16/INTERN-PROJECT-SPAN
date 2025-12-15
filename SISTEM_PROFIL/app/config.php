d671aa6360b2a3a17264f0ecc7b5ddd1611df61b
nurirdina16 <nurirdinasyafiqahab@gmail.com>
2025-12-15 12:35:20 +0800

    updated config

--- SISTEM_PROFIL/app/config.php
+++ SISTEM_PROFIL/app/config.php
@@ -1,34 +1,13 @@
 <?php
 // app/config.php
-//$host = '127.0.0.1';
-//$db   = 'sistem_profil_span';
-//$user = 'root';
-//$pass = ''; // default empty password
-
-//try {
-//    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
-//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//} catch (PDOException $e) {
-//    die("Gagal sambung ke pangkalan data: " . $e->getMessage());
-//}
-
-// app/config.php (UNTUK RENDER)
-
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$host = '127.0.0.1';
+$db   = 'sistem_profil_span';
+$user = 'root';
+$pass = ''; // default empty password
 
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Gagal sambung ke pangkalan data: " . $e->getMessage());
 }
